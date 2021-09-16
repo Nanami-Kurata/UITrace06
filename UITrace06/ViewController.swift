912fba1d8f5aaf13815070b09ec3bff6cda4f62d
Nanami Kurata <krtnnm20011031@gmail.com>
2021-09-16 14:42:09 +0900

    first commit

---
+++ UITrace06/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UITrace06
+//
+//  Created by 倉田七海 on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    
+    
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    
+}
